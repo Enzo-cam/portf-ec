f37f055ff9d539d8a6b2213b4944c52c6a48f54c
EnzoCam7 <camerac09@gmail.com>
2023-06-21 17:09:47 -0300

    Added Container, more components and hard code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,36 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Layout from '../../Components/Layout'
 import Enzocard from '../../Components/Enzocard'
+import { Container, Divider } from '@chakra-ui/react'
+import Projects from '../../Components/Projects'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const projects = [
+  {
+    name: 'EducaIT',
+    description: 'Is a website to search for best courses of development, guides, utils and more.',
+    technologies: ['next', 'typeScript', 'mongo', 'tailwind'],
+  },
+  {
+    name: 'RecipeHub',
+    description: 'Website to find the best recipes based in your ingredients.',
+    technologies: ['next', 'typeScript', 'mongo', 'chakraUI', 'nestJS'],
+  },
+];
 
 export default function Home() {
   return (
     <>
-      <Layout>
-        <Enzocard />
-      </Layout>
+      <Container maxW={{ base: "100%", sm:"50%", md: "43%" }}>
+        <Layout>
+          <Enzocard />
+          <Divider />
+          <Projects 
+            projects={projects}
+          />
+        </Layout>
+      </Container>
     </>
   )
 }
