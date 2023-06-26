935d34ee20e54d36b59adf229c16310ee4908d43
EnzoCam7 <camerac09@gmail.com>
2023-06-25 23:58:32 -0300

    Edited experience component to renderize all data here

--- Components/Experience.js
+++ Components/Experience.js
@@ -1,22 +1,28 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, ListItem, UnorderedList } from '@chakra-ui/react';
 
   export const Experience = ({
     titleJob,
     date,
     nameCompany,
-    children
+    responsabilities
   }) => {
     return (
         <Box display="flex" flexDirection="column" gap={5} mb={4}>
         <Box display="flex" flexDirection="column" gap={1}>
           <Heading as="h2" fontSize="lg" fontWeight="semibold" color="gray.700">
-            {titleJob} - {nameCompany}
+            {titleJob} | {nameCompany}
           </Heading>
           <Text fontSize="sm" color="gray.500">
             {date}
           </Text>
         </Box>
-        {children}
+        <UnorderedList spacing={2} ml={6} color="gray.700" fontSize="lg">
+          {responsabilities.map(resp => (
+            <ListItem>
+              {resp}
+            </ListItem>
+          ))}
+        </UnorderedList>
       </Box>
     )
   }
