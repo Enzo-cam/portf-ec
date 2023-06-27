a55f06bf112beda56e20a2c5cd9c3e55b6827f39
EnzoCam7 <camerac09@gmail.com>
2023-06-27 01:26:04 -0300

    Fixed styles

--- Components/Enzocard.js
+++ Components/Enzocard.js
@@ -8,7 +8,7 @@ const Enzocard = () => {
       flexDirection={{ base: "column", md: "row" }}
       gap={5}
       alignItems={{ base: "center", md: "flex-start" }}
-      mb={6}
+      mb={8}
     >
       <Image
         border="2px black solid"
@@ -17,14 +17,20 @@ const Enzocard = () => {
         boxSize="180px"
         mb={{ base: 1, md: 0 }} // Add bottom margin on small screens
       />
-      <Box textAlign={{base: "center", md:"left"}}>
+      <Box textAlign={{ base: "center", md: "left" }}>
         <Text fontSize="2.5rem" fontWeight="600">
           Enzo Cámera
         </Text>
         <Text mb={4} fontSize="1.2rem">
           Fullstack Developer, focused in frontend technologies.
         </Text>
-        <Box display="flex" alignItems="center" justifyContent={{ base: "center", md: "left" }} gap={4} my={2}>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent={{ base: "center", md: "left" }}
+          gap={4}
+          my={2}
+        >
           <Tooltip
             placement="bottom"
             hasArrow
@@ -37,7 +43,12 @@ const Enzocard = () => {
             </Text>
           </Tooltip>
           <Link href="/Cv-EnzoCamEN.pdf" download="CV-EnzocamEN">
-            <Button backgroundColor="#5E503F"color="white" variant='solid' _hover={{bg: "#5E503F"}}>
+            <Button
+              backgroundColor="#5E503F"
+              color="white"
+              variant="solid"
+              _hover={{ bg: "#5E503F" }}
+            >
               Resume
             </Button>
           </Link>
